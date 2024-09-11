ee094775ce5f273d4d66c94b80b0f5e7176e5521
Shinie Aggarwal <shinieaggarwal72@gmail.com>
2024-09-12 00:13:11 +0530

    Update portfolio.py - changed csv file path

--- app/portfolio.py
+++ app/portfolio.py
@@ -3,7 +3,7 @@ import chromadb
 import uuid
 
 class Portfolio:
-    def __init__(self, file_path = 'app/resource/my_portfolio.csv'):
+    def __init__(self, file_path = 'resource/my_portfolio.csv'):
         self.file_path = file_path
         self.data = pd.read_csv(file_path)
         self.chroma_client = chromadb.PersistentClient('vectorstore')
